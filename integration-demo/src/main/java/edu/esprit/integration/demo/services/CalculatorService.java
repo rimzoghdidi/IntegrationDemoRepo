3c256a87d165b0942e077fb0f3025e383a4f700e
Rim <rim.zoghdidi@esprit.tn>
2014-05-28 14:32:41 +0200

    Signed-off-by: Rim <rim.zoghdidi@esprit.tn>

---
+++ integration-demo/src/main/java/edu/esprit/integration/demo/services/CalculatorService.java
@@ -0,0 +1,5 @@
+package edu.esprit.integration.demo.services;
+
+public class CalculatorService {
+
+}
---
+++ integration-demo/src/test/java/edu/esprit/integration/demo/test/CalculatorTest.java
@@ -0,0 +1,5 @@
+package edu.esprit.integration.demo.test;
+
+public class CalculatorTest {
+
+}
